1e02f71a7fb0f0e9019aa37df828327c1ab4216a
stockholm44 <ytshaha@hanmail.net>
2018-04-24 00:13:55 +0900

    sdfs

--- blog/scrap.py
+++ blog/scrap.py
@@ -14,9 +14,9 @@ def jpy_rate():
     # 1. 환전소 이름찾기
     table_div = soup.find("div",{"class": "box_contents1"})
     spans = table_div.find_all("span",{'class':'bank_name'})
-    bank_name = []
+    bank_name1 = []
     for a_tag in spans:
-        bank_name.append(a_tag.text)
+        bank_name1.append(a_tag.text)
 
     # for i, name in enumerate(bank_name):
     #     print(i, name)
@@ -31,20 +31,20 @@ def jpy_rate():
     # for i in range(len(tr_tag)):
     #     a = tr_tag[i].find_all('td',{'class':'right txt_em '})
 
-    bank_exchange_rate = []
+    bank_exchange_rate1 = []
     for i in range(len(tr_tag)):
         if i == 0:
             a = tr_tag[i].find_all('td',{'class':'right txt_em box'})
             # print(a)
             exchange_rates = a[0].text.split('원')
-            bank_exchange_rate.append(exchange_rates[0])
+            bank_exchange_rate1.append(exchange_rates[0])
         elif i > 0:
             a = tr_tag[i].find_all('td',{'class':'right txt_em '})
             # print(a)
             exchange_rates = a[0].text.split('원')
-            bank_exchange_rate.append(exchange_rates[0])
-    bank_exchange_rate = bank_exchange_rate[:len(bank_name)]
+            bank_exchange_rate1.append(exchange_rates[0])
+    bank_exchange_rate1 = bank_exchange_rate1[:len(bank_name1)]
     # for i, rate in enumerate(bank_exchange_rate):
     #     print(i, rate)
 
-    return bank_name, bank_exchange_rate
+    return bank_name1, bank_exchange_rate1
